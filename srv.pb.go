16b61f35f8c3c72813458873cfe58c46d419e98b
Evgeny Ustinkin <evgeny@ustinkin.com>
2021-09-26 09:59:04 +0300

    GetVersion method

--- srv.pb.go
+++ srv.pb.go
@@ -118,26 +118,101 @@ func (m *SrvResponse) GetResult() string {
 	return ""
 }
 
+type VersionRequest struct {
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *VersionRequest) Reset()         { *m = VersionRequest{} }
+func (m *VersionRequest) String() string { return proto.CompactTextString(m) }
+func (*VersionRequest) ProtoMessage()    {}
+func (*VersionRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_2bbe8325d22c1a26, []int{2}
+}
+
+func (m *VersionRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_VersionRequest.Unmarshal(m, b)
+}
+func (m *VersionRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_VersionRequest.Marshal(b, m, deterministic)
+}
+func (m *VersionRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_VersionRequest.Merge(m, src)
+}
+func (m *VersionRequest) XXX_Size() int {
+	return xxx_messageInfo_VersionRequest.Size(m)
+}
+func (m *VersionRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_VersionRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_VersionRequest proto.InternalMessageInfo
+
+type VersionResponse struct {
+	Result               string   `protobuf:"bytes,1,opt,name=result,proto3" json:"result,omitempty"`
+	XXX_NoUnkeyedLiteral struct{} `json:"-"`
+	XXX_unrecognized     []byte   `json:"-"`
+	XXX_sizecache        int32    `json:"-"`
+}
+
+func (m *VersionResponse) Reset()         { *m = VersionResponse{} }
+func (m *VersionResponse) String() string { return proto.CompactTextString(m) }
+func (*VersionResponse) ProtoMessage()    {}
+func (*VersionResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_2bbe8325d22c1a26, []int{3}
+}
+
+func (m *VersionResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_VersionResponse.Unmarshal(m, b)
+}
+func (m *VersionResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_VersionResponse.Marshal(b, m, deterministic)
+}
+func (m *VersionResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_VersionResponse.Merge(m, src)
+}
+func (m *VersionResponse) XXX_Size() int {
+	return xxx_messageInfo_VersionResponse.Size(m)
+}
+func (m *VersionResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_VersionResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_VersionResponse proto.InternalMessageInfo
+
+func (m *VersionResponse) GetResult() string {
+	if m != nil {
+		return m.Result
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*SrvRequest)(nil), "SrvRequest")
 	proto.RegisterType((*SrvResponse)(nil), "SrvResponse")
+	proto.RegisterType((*VersionRequest)(nil), "VersionRequest")
+	proto.RegisterType((*VersionResponse)(nil), "VersionResponse")
 }
 
 func init() { proto.RegisterFile("srv.proto", fileDescriptor_2bbe8325d22c1a26) }
 
 var fileDescriptor_2bbe8325d22c1a26 = []byte{
-	// 170 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x2c, 0x2e, 0x2a, 0xd3,
-	0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x57, 0x8a, 0xe1, 0xe2, 0x0a, 0x2e, 0x2a, 0x0b, 0x4a, 0x2d, 0x2c,
-	0x4d, 0x2d, 0x2e, 0x11, 0x92, 0xe3, 0xe2, 0xca, 0x4d, 0x2d, 0xc9, 0xc8, 0x4f, 0xf1, 0x4b, 0xcc,
-	0x4d, 0x95, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0c, 0x42, 0x12, 0x11, 0x12, 0xe1, 0x62, 0xcd, 0xcd,
-	0x4f, 0x49, 0xcd, 0x91, 0x60, 0x02, 0x4b, 0x41, 0x38, 0x42, 0x12, 0x5c, 0xec, 0x65, 0xa9, 0x45,
-	0xc5, 0x99, 0xf9, 0x79, 0x12, 0xcc, 0x60, 0x71, 0x18, 0x57, 0x49, 0x95, 0x8b, 0x1b, 0x6c, 0x7a,
-	0x71, 0x41, 0x7e, 0x5e, 0x71, 0xaa, 0x90, 0x18, 0x17, 0x5b, 0x51, 0x6a, 0x71, 0x69, 0x4e, 0x09,
-	0xd4, 0x68, 0x28, 0xcf, 0xc8, 0x14, 0xec, 0x88, 0xe0, 0xd4, 0xa2, 0xb2, 0xcc, 0xe4, 0x54, 0x21,
-	0x75, 0x2e, 0x0e, 0xe7, 0xc4, 0x9c, 0x1c, 0xb7, 0xd2, 0xbc, 0x64, 0x21, 0x6e, 0x3d, 0x84, 0xeb,
-	0xa4, 0x78, 0xf4, 0x90, 0x0c, 0x53, 0x62, 0x48, 0x62, 0x03, 0x7b, 0xc1, 0x18, 0x10, 0x00, 0x00,
-	0xff, 0xff, 0xf3, 0x4e, 0xf2, 0x9f, 0xcf, 0x00, 0x00, 0x00,
+	// 209 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x7c, 0x50, 0x4d, 0x4b, 0x85, 0x40,
+	0x14, 0xd5, 0x22, 0xcb, 0x6b, 0xa4, 0x5c, 0x22, 0x06, 0x17, 0x11, 0x03, 0x51, 0x6d, 0x06, 0xaa,
+	0x9f, 0x10, 0xd4, 0xae, 0x85, 0x42, 0xab, 0x36, 0xa6, 0x17, 0x14, 0x46, 0xc7, 0x66, 0xc6, 0xf9,
+	0xfd, 0xc1, 0x38, 0x95, 0xef, 0x2d, 0xde, 0xf2, 0x9c, 0xcb, 0xf9, 0xba, 0x90, 0x1a, 0xed, 0xc4,
+	0xac, 0x95, 0x55, 0xfc, 0x13, 0xa0, 0xd6, 0xae, 0xa2, 0xef, 0x85, 0x8c, 0xc5, 0x6b, 0x80, 0x91,
+	0x6c, 0xaf, 0xba, 0xf7, 0x66, 0x24, 0x16, 0xdf, 0xc4, 0xf7, 0x69, 0xb5, 0x61, 0xf0, 0x12, 0x4e,
+	0x46, 0xd5, 0x91, 0x64, 0x47, 0xfe, 0xb4, 0x02, 0x64, 0x70, 0xea, 0x48, 0x9b, 0x41, 0x4d, 0xec,
+	0xd8, 0xf3, 0xbf, 0x90, 0xdf, 0x42, 0xe6, 0xdd, 0xcd, 0xac, 0x26, 0x43, 0x78, 0x05, 0x89, 0x26,
+	0xb3, 0x48, 0x1b, 0xac, 0x03, 0xe2, 0x05, 0x5c, 0x7c, 0xac, 0x8a, 0x50, 0x84, 0x3f, 0x40, 0xfe,
+	0xc7, 0x1c, 0x16, 0x3f, 0xf5, 0x7e, 0x41, 0x4d, 0xda, 0x0d, 0x2d, 0xe1, 0x1d, 0x9c, 0xbd, 0x34,
+	0x52, 0xbe, 0x2e, 0x53, 0x8b, 0x99, 0xf8, 0x9f, 0x56, 0x9e, 0x8b, 0x4d, 0x13, 0x1e, 0xe1, 0x23,
+	0xc0, 0x1b, 0xd9, 0x10, 0x82, 0xb9, 0xd8, 0x2d, 0x50, 0x16, 0x62, 0x2f, 0x9f, 0x47, 0x5f, 0x89,
+	0x7f, 0xd9, 0xf3, 0x4f, 0x00, 0x00, 0x00, 0xff, 0xff, 0xaa, 0x4c, 0x5e, 0x26, 0x3f, 0x01, 0x00,
+	0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -153,6 +228,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type SrvServiceClient interface {
 	CallFunc(ctx context.Context, in *SrvRequest, opts ...grpc.CallOption) (*SrvResponse, error)
+	GetVersion(ctx context.Context, in *VersionRequest, opts ...grpc.CallOption) (*VersionResponse, error)
 }
 
 type srvServiceClient struct {
@@ -172,9 +248,19 @@ func (c *srvServiceClient) CallFunc(ctx context.Context, in *SrvRequest, opts ..
 	return out, nil
 }
 
+func (c *srvServiceClient) GetVersion(ctx context.Context, in *VersionRequest, opts ...grpc.CallOption) (*VersionResponse, error) {
+	out := new(VersionResponse)
+	err := c.cc.Invoke(ctx, "/SrvService/GetVersion", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // SrvServiceServer is the server API for SrvService service.
 type SrvServiceServer interface {
 	CallFunc(context.Context, *SrvRequest) (*SrvResponse, error)
+	GetVersion(context.Context, *VersionRequest) (*VersionResponse, error)
 }
 
 // UnimplementedSrvServiceServer can be embedded to have forward compatible implementations.
@@ -183,6 +269,9 @@ type UnimplementedSrvServiceServer struct {
 
 func (*UnimplementedSrvServiceServer) CallFunc(ctx context.Context, req *SrvRequest) (*SrvResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method CallFunc not implemented")
+}
+func (*UnimplementedSrvServiceServer) GetVersion(ctx context.Context, req *VersionRequest) (*VersionResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method GetVersion not implemented")
 }
 
 func RegisterSrvServiceServer(s *grpc.Server, srv SrvServiceServer) {
@@ -207,6 +296,24 @@ func _SrvService_CallFunc_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+func _SrvService_GetVersion_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(VersionRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(SrvServiceServer).GetVersion(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/SrvService/GetVersion",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(SrvServiceServer).GetVersion(ctx, req.(*VersionRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _SrvService_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "SrvService",
 	HandlerType: (*SrvServiceServer)(nil),
@@ -214,6 +321,10 @@ var _SrvService_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "CallFunc",
 			Handler:    _SrvService_CallFunc_Handler,
+		},
+		{
+			MethodName: "GetVersion",
+			Handler:    _SrvService_GetVersion_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
